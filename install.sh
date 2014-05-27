c9bf5a76962592f331a617f4ca6a489cc1addb2f
pi0 <pyapar@gmail.com>
2014-05-27 09:47:54 -0400

    bug fix

--- install.sh
+++ install.sh
@@ -2,20 +2,14 @@
 
 base=/usr/share/dotfiles
 
-if [ ! -d $base ] ; then
-        sudo -E git clone https://github.com/pi0/dotfiles.git $base ;
-    else
-        sudo -E git --git-dir=$base/.git pull;
-    fi
-
 rm -rv $HOME/.aria2
 rm -rv  $HOME/.vim
 touch  $HOME/.z
 
-for f in $base/.* ; do
-	$name=`basename $f`
-	echo "Installing $name..."
-	ln -sf $f $HOME/$name
+for f in $base/.* 
+do
+	echo "Installing $(basename $f)..."
+	ln -sf $f $HOME
 done
 unlink $HOME/.git
 
