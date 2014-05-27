03523d915c090dbeacb8f4aea0d8d03f9b135266
Pooya parsa <pi0@users.noreply.github.com>
2014-05-27 17:42:33 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@
 
 base=/usr/share/dotfiles
 
+if [ ! -d $base ] ; then
+        sudo -E git clone https://github.com/pi0/dotfiles.git $base ;
+    else
+        sudo -E git --git-dir=$base/.git pull;
+    fi
+
 rm -rv ~/.aria2
 rm -rv ~/.vim
 
